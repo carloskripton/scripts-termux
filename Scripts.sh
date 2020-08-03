393fca11eb7cc3bf88e3a58268705785542e9b0e
carloskripton <69091362+carloskripton@users.noreply.github.com>
2020-08-03 08:34:20 -0500

    Create Scripts.sh

---
+++ Scripts.sh
@@ -0,0 +1,120 @@
+#!bin/bash
+#by:scripts.sh
+
+echo -e "\e[32m NINGUN SISTEMA ES SEGURO \e[0m"
+echo -e "\e[32m SCRIPT SIMPLE PERO UTIL :V\e[0m"
+sleep 2
+echo -e "\e[1;31m----------♧----------\e[0m"
+echo -e "\e[1;31m|      KRIPTON      |\e[0m"
+echo -e "\e[1;31m| scripts de termux |\e[0m"
+echo -e "\e[1;31m----------♤----------\e[0m"
+echo -e ""
+sleep 4
+echo -e "\e[1;31m CLAY \e[0m"
+echo -e "\e[31m scripts mas usados en termux\e[0m"
+sleep 5
+clear
+echo -e "\e[32m 📱¿Qué es sendmail?📱
+
+sendmail es una herramienta que nos permite enviar mensajes al correo elec>
+
+📱INSTALACIÓN DE sendmail EN TERMUX (ANDROID)📱
+
+apt update && apt upgrade -y
+
+termux-setup-storage
+
+pkg install -y git
+
+pkg install -y pv
+
+pkg install -y curl
+
+pkg install -y figlet
+
+git clone https://github.com/Fabr1x/sendmail
+
+cd sendmail
+
+chmod 711 sendmail.sh
+
+./sendmail.sh\e[0m"
+
+echo -e ""
+
+echo -e "\e[34m📱¿Qué es HiddenEye?📱
+
+HiddenEye es una herramienta de hacking todo en uno que incluye Keylogger,>
+
+📱INSTALACIÓN DE HiddenEye EN TERMUX (ANDROID ROOT)📱
+
+apt update && apt upgrade -y
+
+termux-setup-storage
+
+pkg install -y python
+
+pkg install -y python2
+
+pkg install -y grep
+
+pkg install -y git
+
+pkg install -y openssh
+
+pkg install -y php
+
+pkg install -y curl
+
+pkg install -y wget
+
+git clone https://github.com/DarkSecDevelopers/HiddenEye
+
+cd HiddenEye
+
+pip install -r requirements.txt
+
+python HiddenEye.py\e[0m"
+
+echo -e ""
+
+echo -e "\e[31m👉crea un virus troyano con termux en android📱 facil y rap>
+
+Nota:no instales el virus en tu celular 📱,por qué también te afectara.
+
+BY:꧁ﾫ⃞ﾫ Ⲙ꯭₳€꯭Ħ꯭₳꯭€Ԟ꯭Ɨ₦™ﾫ⃞ﾫ ꧂
+
+---------------9 COMANDOS-------
+$git clone https://github.com/Cesar-Hacker/binarios
+
+$ls
+
+$cd binarios
+
+$ls
+
+$chmod 777 install.sh
+
+$ls
+
+$bash install.sh
+
+--------_-------------CESAR HACK--------------------
+
+$binarios
+
+Que binario quieres?
+
+$1
+
+-------------------listo ahora está en tu memoria interna---------
+
+☝️
+ Una vez no haya aparecido el mensaje de arriba☝️,Salimos de la app (termux>
+
+echo -e ""
+
+echo -e "\e[32m NO CONTAMOS CON MUCHOS PERO PRONTO\e[0m"
+echo -e "\e[31m GRASIAS Y HASTA LUEGO\e[0m"
+
+echo -e "\e[31m 👾👾👾👾👾👾👾 \e[0m"
